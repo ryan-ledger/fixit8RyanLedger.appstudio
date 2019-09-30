307df01e36dec2cdc7aba50ee5f8bcdda7bb105a
Ryan Ledger <ryanledger@creighton.edu>
2019-09-29 21:27:53 -0500

    Initial commit

---
+++ forms/namesExtra/namesExtra.js
@@ -0,0 +1,4 @@
+names = ['Bob', 'Janet', 'Tom', 'Bob', 'Randy', 'elizabeth', 'Kim', 'Nancy'] 
+  for (i= names.length - 1; i >= 1 ;i--)
+    console.log(names[i])
+  
---
+++ forms/whileExtra/whileExtra.js
@@ -0,0 +1,7 @@
+name = prompt ('what is your name (First and Last)')
+  let i=0
+  while (i < name.length){
+    console.log(`Letter ${i+1} is: ${name[i]}`)
+    i++
+    }
+  
